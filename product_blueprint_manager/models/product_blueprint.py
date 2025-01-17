3d85f1c83975ab39459644d8625249a24862523e
Antonio <zurbaran79@hotmail.com>
2025-01-17 09:57:45 +0100

    Merge branch 'develop' of github.com:zurbaran/odoo_metal_manufacturing into develop

--- product_blueprint_manager/models/product_blueprint.py
+++ product_blueprint_manager/models/product_blueprint.py
@@ -18,8 +18,7 @@ class ProductBlueprint(models.Model):
 
 class ProductBlueprintFormula(models.Model):
     _name = 'product.blueprint.formula'
-    _description = 'Fórmula del Producto'
-
+    _description = 'Fórmula del Plano de Producto'
     COLOR_OPTIONS = [
         ('black', 'Negro'),
         ('white', 'Blanco'),
@@ -35,21 +34,22 @@ class ProductBlueprintFormula(models.Model):
     name = fields.Char('Nombre de la Fórmula', required=True)
     formula_expression = fields.Char('Expresión de la Fórmula', required=True)
     product_id = fields.Many2one('product.template', string='Producto', required=True)
-    blueprint_id = fields.Many2one('product.blueprint', string='Plano', required=True, help="El plano al que pertenece esta fórmula.")
+    blueprint_id = fields.Many2one('product.blueprint', string='Plano', required=True, help="El plano al que pertenece esta fórmula.", ondelete='cascade')
     position_x = fields.Float('Posición X', required=True, help="La coordenada X para posicionar la fórmula en el plano.")
     position_y = fields.Float('Posición Y', required=True, help="La coordenada Y para posicionar la fórmula en el plano.")
     available_attributes = fields.Char(string="Atributos Disponibles", compute="_compute_available_attributes", store=False, help="Atributos disponibles para usar en la fórmula (separados por comas).")
-    font_size = fields.Float('Tamaño de la Fuente', default=12.0)
+    font_size = fields.Char('Tamaño de la fuente', default='24px')
     font_color = fields.Selection(COLOR_OPTIONS, string='Color de la Fuente', default='black', help="Nombre del color de la fuente.")
 
     @api.depends('product_id')
     def _compute_available_attributes(self):
-        """Compute available attributes for use in the formula."""
         for record in self:
             if record.product_id:
-                custom_attribute_values = record.product_id.valid_product_template_attribute_line_ids.mapped('attribute_id.value_ids').filtered(lambda v: v.is_custom)
-                variable_names = [value.name for value in custom_attribute_values]
-                record.available_attributes = ', '.join(variable_names)
+                # Obtiene todos los atributos personalizados para la plantilla de producto
+                custom_attributes = record.product_id.attribute_line_ids.filtered(
+                    lambda line: line.attribute_id.create_variant == 'no_variant'
+                ).mapped('attribute_id')
+                record.available_attributes = ', '.join(custom_attributes.mapped('name'))
             else:
                 record.available_attributes = ''
 
