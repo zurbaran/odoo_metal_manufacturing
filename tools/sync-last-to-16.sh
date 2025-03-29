448bf782abe59e0b039297122a4f8dfdcc77ef1c
Antonio <zurbaran79@hotmail.com>
2025-03-29 10:42:44 +0100

    Cherry-pick c2686246b91e728d784c41e51db2dae09cdf6988 desde develop sin modificar __manifest__.py

--- tools/sync-last-to-16.sh
+++ tools/sync-last-to-16.sh
@@ -9,7 +9,16 @@ LAST_COMMIT=$(git log develop -1 --pretty=format:"%H")
 echo ">> Último commit en develop: $LAST_COMMIT"
 
 git checkout 16.0 || exit 1
-git cherry-pick "$LAST_COMMIT" || exit 1
+git cherry-pick -n "$LAST_COMMIT" || exit 1
+
+# Restaurar los dos __manifest__.py
+git restore --staged product_blueprint_manager/__manifest__.py
+git restore --staged product_configurator_attribute_price/__manifest__.py
+
+git restore product_blueprint_manager/__manifest__.py
+git restore product_configurator_attribute_price/__manifest__.py
+
+git commit -m "Cherry-pick $LAST_COMMIT desde develop sin modificar __manifest__.py"
 
 echo ">> Haciendo push a origin/16.0"
 git push origin 16.0
