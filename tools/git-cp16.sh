be8d8ec05eda2c937a15bd80d32bef46ff5090f5
Antonio <zurbaran79@hotmail.com>
2025-03-29 10:42:44 +0100

    Cherry-pick c2686246b91e728d784c41e51db2dae09cdf6988 desde develop sin modificar __manifest__.py

--- tools/git-cp16.sh
+++ tools/git-cp16.sh
@@ -3,10 +3,20 @@
 COMMIT="$1"
 
 if [ -z "$COMMIT" ]; then
-  echo "Uso: ./tools/git-cp16.sh <commit_hash>"
+  echo "Uso: ./tools/git-cp17.sh <commit_hash>"
   exit 1
 fi
 
-echo ">> Cherry-pickeando $COMMIT desde develop a 16.0"
-git checkout 16.0 &&
-git cherry-pick "$COMMIT"
+echo ">> Cherry-pickeando $COMMIT desde develop a 16.0 (sin modificar los manifests)"
+
+git checkout 16.0 || exit 1
+git cherry-pick -n "$COMMIT" || exit 1
+
+# Restaurar ambos manifests
+git restore --staged product_blueprint_manager/__manifest__.py
+git restore --staged product_configurator_attribute_price/__manifest__.py
+
+git restore product_blueprint_manager/__manifest__.py
+git restore product_configurator_attribute_price/__manifest__.py
+
+git commit -m "Cherry-pick $COMMIT desde develop sin modificar __manifest__.py"
