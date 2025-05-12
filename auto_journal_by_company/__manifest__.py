278d76e9daebc0a4ac573f982ae16cc40993c4f8
Antonio <zurbaran79@hotmail.com>
2025-05-12 19:05:25 +0200

    add auto_journal_by_company

---
+++ auto_journal_by_company/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Auto Journal by Company",
+    'version': '17.0.1.0.0',
+    "category": "Accounting",
+    "summary": "Set default journal based on company and invoice type",
+    'description': '''
+    ''',
+    'author': 'Antonio Caballero',
+    'maintainer': 'Antonio Caballero',
+    'website': 'https://github.com/zurbaran/odoo_metal_manufacturing',
+    'depends': ['account'],
+    "data": [
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
