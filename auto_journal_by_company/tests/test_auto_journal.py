9e7fbc9d8600230c8cb391679fa510404783b1b0
Antonio <zurbaran79@hotmail.com>
2025-05-12 19:05:43 +0200

    Cherry-pick 6ad72e9857fc8310d44ab7eb16d27e9b370bcd77 desde develop sin modificar __manifest__.py

---
+++ auto_journal_by_company/tests/test_auto_journal.py
@@ -0,0 +1,36 @@
+from odoo.tests import TransactionCase
+
+class TestAutoJournalByCompany(TransactionCase):
+
+    def setUp(self):
+        super().setUp()
+        self.company = self.env['res.company'].create({
+            'name': 'Empresa de Test',
+        })
+        self.journal_sale = self.env['account.journal'].create({
+            'name': 'Ventas Test',
+            'type': 'sale',
+            'code': 'VT',
+            'company_id': self.company.id,
+        })
+        self.journal_purchase = self.env['account.journal'].create({
+            'name': 'Compras Test',
+            'type': 'purchase',
+            'code': 'CT',
+            'company_id': self.company.id,
+        })
+
+    def test_auto_journal_sale_invoice(self):
+        move = self.env['account.move'].create({
+            'move_type': 'out_invoice',
+            'company_id': self.company.id,
+        })
+        self.assertEqual(move.journal_id, self.journal_sale, "No se asignó correctamente el diario de ventas")
+
+    def test_auto_journal_purchase_invoice(self):
+        move = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'company_id': self.company.id,
+        })
+        self.assertEqual(move.journal_id, self.journal_purchase, "No se asignó correctamente el diario de compras")
+ 
