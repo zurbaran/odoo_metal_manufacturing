22529e789c57465af254125a8863137a1bf2d21e
Antonio <zurbaran79@hotmail.com>
2025-06-04 17:35:52 +0200

    Eliminada informacion inecesaria del reporte
    Se han eliminado los titulos del reporte plano de fabricacion para
    reducir informacion.

--- product_blueprint_manager/__manifest__.py
+++ product_blueprint_manager/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Blueprint Manager',
-    'version': '17.0.5.0.2',
+    'version': '17.0.5.0.3',
     'category': 'Manufacturing',
     'summary': 'Gestione planos de productos y genere documentos de forma dinámica.',
     'description': '''
