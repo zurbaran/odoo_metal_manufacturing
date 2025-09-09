af442e4fc4588809b5b8514f9bce2f5a4f9cd652
antonio <zurbaran79@hotmail.com>
2025-09-09 12:26:20 +0200

    [IMP] product_blueprint_manager: vistas listas para Blueprints y Fórmulas
    - Se añade nueva vista de lista para Blueprints que muestra:
      * Nombre del plano
      * Producto asociado
      * Tipo de plano
    
    - Se añade nueva vista de lista para Fórmulas que muestra:
      * Nombre de la fórmula
      * Producto asociado
      * Plano asociado
      * Expresión de la fórmula
      * Estilos de color y fuente (opcionales)
    
    - Se actualiza el manifest para cargar las nuevas vistas y se incrementa la versión a 18.0.10.1.4
    - Se ajusta el __init__.py de views para incluir las nuevas vistas
    
    (cherry picked from commit ac7c035454399f93bb22abb617eaa8e401933bef)

--- product_blueprint_manager/views/__init__.py
+++ product_blueprint_manager/views/__init__.py
@@ -2,6 +2,8 @@ from . import (
     blueprint_report_template,
     menu_views,
     product_views,
+    product_blueprint_views,
+    product_blueprint_formula_views,
     product_blueprint_condition_views,
     sale_order_views,
     sale_report_blueprint_final,
