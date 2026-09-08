5f4a12f6df9c16e8d970387632cafaecce420592
Antonio <zurbaran79@hotmail.com>
2026-09-08 11:29:12 +0200

    [TEST] product_configurator_attribute_price: cover ACLs and formula sandbox

---
+++ product_configurator_attribute_price/tests/test_security.py
@@ -0,0 +1,43 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestConfiguratorSecurity(TransactionCase):
+    def test_custom_acl_is_not_granted_to_all_internal_users(self):
+        system_group = self.env.ref("base.group_system")
+        for xmlid in (
+            "product_configurator_attribute_price.access_product_template_attribute_value",
+            "product_configurator_attribute_price.access_sale_order_line",
+        ):
+            access = self.env.ref(xmlid)
+            self.assertEqual(access.group_id, system_group)
+
+    def test_safe_formula_evaluation(self):
+        model = self.env["product.template.attribute.value"]
+        self.assertEqual(
+            model._safe_eval(
+                "((custom_value + 49) // 50 - 10) * 5",
+                {"custom_value": 700, "price_so_far": 1000},
+            ),
+            20,
+        )
+        self.assertEqual(
+            model._safe_eval(
+                "price_so_far * 0.2",
+                {"custom_value": 700, "price_so_far": 1000},
+            ),
+            200.0,
+        )
+
+    def test_unsafe_formula_is_rejected(self):
+        model = self.env["product.template.attribute.value"]
+        attacks = (
+            "(1).__class__.__mro__",
+            "__import__('os').system('id')",
+            "custom_value.__class__",
+        )
+        for expression in attacks:
+            with self.assertRaises(ValueError, msg=expression):
+                model._safe_eval(
+                    expression,
+                    {"custom_value": 700, "price_so_far": 1000},
+                )
